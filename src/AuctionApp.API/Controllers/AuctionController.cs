2b152aa7078c82fbc426ba2a89480df05055aa24
antonioRenato <antoniorpfs@gmail.com>
2024-11-17 20:50:58 -0300

    Add project files.

---
+++ src/AuctionApp.API/Controllers/AuctionController.cs
@@ -0,0 +1,20 @@
+﻿using AuctionApp.API.UseCases.Auctions.GetCurrent;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuctionApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuctionController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetCurrentAuction()
+        {
+            var useCase = new GetCurrentAuctionUseCase();
+
+            var result = useCase.Execute();
+            
+            return Ok(result);
+        }
+    }
+}
---
+++ src/AuctionApp.API/UseCases/Auctions/GetCurrent/GetCurrentAuctionUseCase.cs
@@ -0,0 +1,18 @@
+﻿using AuctionApp.API.Entities;
+
+namespace AuctionApp.API.UseCases.Auctions.GetCurrent
+{
+    public class GetCurrentAuctionUseCase
+    {
+        public Auction Execute()
+        {
+            return new Auction
+            {
+                Id = 1,
+                Name = "Test",
+                Starts = DateTime.Now,
+                Ends = DateTime.Now,
+            };
+        }
+    }
+}
